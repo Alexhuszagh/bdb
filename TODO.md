5a638b901509945b16b72dc3a7c4375ed7fd40b8
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2018-10-24 16:54:24 -0500

    Added benchmarks for valid and complete.

--- TODO.md
+++ TODO.md
@@ -1,5 +1,11 @@
 # TODO List
 
+# Benchmarks
+    - Need to add benchmarks for UniProt serialization/deserialization
+    - CSV (small, but high-level only)
+    - Fasta (small, but high-level only)
+    - XML (small, but high-level only)
+
 # Formats
     - Add MGF, mzXML, mzML, etc....
     - Going to need XML support.
---
+++ benches/uniprot/complete.rs
@@ -0,0 +1,42 @@
+#[macro_use]
+extern crate bencher;
+extern crate bdb;
+
+use bencher::{black_box, Bencher};
+use bdb::db::uniprot::*;
+use bdb::traits::*;
+
+// BENCHES
+
+fn complete(bench: &mut Bencher) {
+    let gapdh = Record {
+        sequence_version: 3,
+        protein_evidence: ProteinEvidence::ProteinLevel,
+        mass: 35780,
+        length: 333,
+        gene: String::from("GAPDH"),
+        id: String::from("P46406"),
+        mnemonic: String::from("G3P_RABIT"),
+        name: String::from("Glyceraldehyde-3-phosphate dehydrogenase"),
+        organism: String::from("Oryctolagus cuniculus"),
+        proteome: String::from("UP000001811"),
+        sequence: b"MVKVGVNGFGRIGRLVTRAAFNSGKVDVVAINDPFIDLHYMVYMFQYDSTHGKFHGTVKAENGKLVINGKAITIFQERDPANIKWGDAGAEYVVESTGVFTTMEKAGAHLKGGAKRVIISAPSADAPMFVMGVNHEKYDNSLKIVSNASCTTNCLAPLAKVIHDHFGIVEGLMTTVHAITATQKTVDGPSGKLWRDGRGAAQNIIPASTGAAKAVGKVIPELNGKLTGMAFRVPTPNVSVVDLTCRLEKAAKYDDIKKVVKQASEGPLKGILGYTEDQVVSCDFNSATHSSTFDAGAGIALNDHFVKLISWYDNEFGYSNRVVDLMVHMASKE".to_vec(),
+        taxonomy: String::from("9986"),
+        reviewed: true,
+    };
+
+    bench.iter(|| { black_box(gapdh.is_complete()) })
+}
+
+fn incomplete(bench: &mut Bencher) {
+    let empty = Record::new();
+
+    bench.iter(|| { black_box(!empty.is_complete()) })
+}
+
+benchmark_group!(
+    benches,
+    complete,
+    incomplete
+);
+benchmark_main!(benches);
